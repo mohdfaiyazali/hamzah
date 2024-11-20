8cd9d03a9493cbd5802a3ddcaef8ba0af3806906
Mohd Faiyaz Ali <71717503+mohdfaiyazali@users.noreply.github.com>
2024-11-20 15:37:06 +0530

    Create first.py
    this is my first program

---
+++ first.py
@@ -0,0 +1,7 @@
+def login_status(username,password):
+    if username == "faiyaz" and password == "hamzah":
+        print("login success")     #instead of return i used print()
+    else:
+        print("login fail")         
+login_status("faiyaz","hamzah")
+
